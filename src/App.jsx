3ed4912cd925c68f8f182fae48834a9a406ac6e5
Nico <jndr051701@gmail.com>
2023-10-17 21:47:11 -0600

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,20 @@ import "./App.css";
 import Todo from "./components/Todo.jsx";
 import Title from "./components/Title.jsx";
 import Modal from "./components/Modal.jsx";
-import Counter from "./components/Counter.jsx";
 import React, { useState, useEffect } from "react";
+
 
 function App() {
   const [showModal, setShowModal] = useState(false);
-
   function onTodoDelete() {
     setShowModal(true);
   }
-
   function cancelModal() {
     setShowModal(false);
   }
-
   function confirmModal() {
     setShowModal(false);
   }
-
   useEffect(() => {
     console.log('ONLY on mount')
   }, [])
@@ -27,11 +23,9 @@ function App() {
   useEffect(() => {
     console.log(`on mount AND on ${showModal} change`)
   }, [showModal])
-
   useEffect(() => {
     console.log('EVERY render')
   })
-
 
 
 
